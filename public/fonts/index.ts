fd0816ac7e6dc0295a02affb8ea730cdc24793b1
BinZ <sikatunar@gmail.com>
2025-01-14 01:47:54 +0800

    Create index.ts

---
+++ public/fonts/index.ts
@@ -0,0 +1,14 @@
+import { Gasoek_One, REM, Phudu } from "next/font/google";
+
+export const gasoek_one = Gasoek_One({
+  subsets: ["latin"],
+  weight: ["400"],
+});
+
+export const rem = REM({
+  subsets: ["latin"],
+});
+
+export const phudu = Phudu({
+  subsets: ["latin"],
+});
