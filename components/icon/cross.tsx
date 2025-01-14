cc283e9d14c8b512aae3a39d704151959326147d
BinZ <sikatunar@gmail.com>
2025-01-14 15:11:13 +0800

    Create cross.tsx

---
+++ components/icon/cross.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const CrossIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="17"
+      viewBox="0 0 16 17"
+      fill="none"
+    >
+      <g opacity="0.5">
+        <path
+          d="M12.6673 3.83325L3.33398 13.1666M3.33398 3.83325L12.6673 13.1666"
+          stroke="white"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </g>
+    </svg>
+  );
+};
+
+export default CrossIcon;
