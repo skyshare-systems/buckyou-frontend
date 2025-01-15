47ad4ae2a8c0e8bcf48106688be385000b77272f
BinZ <sikatunar@gmail.com>
2025-01-16 01:49:18 +0800

    Create arrow-up-right.tsx

---
+++ components/icon/arrow-up-right.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const ArrowUpRight = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="17"
+      viewBox="0 0 16 17"
+      fill="none"
+    >
+      <path d="M11.3333 5.16666L4 12.5" stroke="white" stroke-linecap="round" />
+      <path
+        d="M7.3335 4.58769C7.3335 4.58769 11.0892 4.2711 11.6592 4.84104C12.2291 5.41098 11.9124 9.16668 11.9124 9.16668"
+        stroke="white"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default ArrowUpRight;
