0bba66d9af395e2856001634404da253010a3310
Binz <sikatunar@gmail.com>
2025-01-16 00:06:10 +0800

    fix

--- views/main-view/page.tsx
+++ views/main-view/page.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import BuyEnvelopes from "./buy-envelopes";
 import ReferFriends from "./refer-friends";
+import TableView from "./table-view";
 
 const MainView = () => {
   return (
     <div className="flex flex-col sm:flex-row md:flex-row gap-2  items-center lg:items-start justify-center lg:justify-between max-w-[512px] sm:max-w-full w-full">
       <BuyEnvelopes />
+
+      <TableView />
       <ReferFriends />
     </div>
   );
---
+++ views/main-view/table-view.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TableView = () => {
+  return (
+    <div className="flex justify-center items-start rounded-2xl border border-white-16 bg-white-4 self-stretch grow">
+      <div className="p-8 min-h-[133px] grow"></div>
+    </div>
+  );
+};
+
+export default TableView;
