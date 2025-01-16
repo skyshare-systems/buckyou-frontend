535a05d714d5bf49c9cd4af28a85fcc6df793358
BinZ <sikatunar@gmail.com>
2025-01-16 23:43:05 +0800

    add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,90 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "@/components/navbar";
+import {
+  SITE_DESCRIPTION,
+  SITE_NAME,
+  SITE_TITLE,
+  SITE_URL,
+} from "@/config/site";
 
 export const metadata: Metadata = {
-  title: "Buck you",
-  description: "Buck You",
+  metadataBase: new URL(SITE_URL),
+  title: {
+    default: SITE_TITLE,
+    template: `%s | ${SITE_TITLE}`,
+  },
+  description: SITE_DESCRIPTION,
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+  manifest: "/favicon/site.webmanifest",
+  twitter: {
+    card: "summary_large_image",
+    title: SITE_NAME,
+    description: SITE_DESCRIPTION,
+    site: "@buckyou",
+    creator: "@buckyou",
+    images: [
+      {
+        url: `${SITE_URL}/assets/ogb.png`,
+        width: 2400,
+        height: 1260,
+        alt: SITE_DESCRIPTION,
+      },
+    ],
+  },
+  creator: "Buck You",
+  openGraph: {
+    url: SITE_URL,
+    type: "website",
+    title: SITE_TITLE,
+    siteName: SITE_TITLE,
+    description: SITE_DESCRIPTION,
+    locale: "en-US",
+    images: [
+      {
+        url: `${SITE_URL}/assets/og/og.png`,
+        width: 2400,
+        height: 1260,
+        alt: SITE_DESCRIPTION,
+        type: "image/png",
+      },
+    ],
+  },
+  icons: {
+    icon: "/favicon/favicon.ico",
+    shortcut: "/favicon/favicon.ico",
+    apple: [
+      {
+        url: "/favicon/apple-touch-icon.png",
+        sizes: "180x180",
+        type: "image/png",
+      },
+    ],
+    other: [
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "16x16",
+        url: "/favicon/favicon-16x16.png",
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "32x32",
+        url: "/favicon/favicon-32x32.png",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
---
+++ config/site.ts
@@ -0,0 +1,9 @@
+export const SITE_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://buckyou-frontend.vercel.app/"
+    : "http://localhost:3000";
+
+export const SITE_NAME = "Buckyou";
+export const SITE_TITLE = "Buckyou";
+export const SITE_DESCRIPTION =
+  "Envelope price increases 1 sui everyday! buy now before it's too late!";
